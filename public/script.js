1a16ad584369c548c379be57b66ce7b7c7bbd3c1
Sumanth Yedoti <sumanth.yedoti@gmail.com>
2022-03-29 17:35:53 +0530

    feat: animate ketchup bottle

--- public/script.js
+++ public/script.js
@@ -1,10 +1,19 @@
 const body = document.querySelector("body")
 const openButton = document.querySelector(".opener")
 const bottle = document.querySelector(".bottle")
+const bottleWrapper = document.querySelector(".bottle-wrapper")
+const cap = document.querySelector(".bottle__cap")
+const capWrapper = document.querySelector(".cap-wrapper")
 
 const handleOpen = () => {
+  cap.classList.add("rotate-cap")
+  capWrapper.classList.add("move-cap")
+  bottle.classList.add("rotate-bottle")
+  bottleWrapper.classList.add("shake-bottle")
+  setTimeout(() => {
     openButton.classList.add("hide")
     bottle.classList.add("hide")
     body.classList.add("ketchup-bg")
+  }, 11000)
 }
 openButton.addEventListener("click", handleOpen)
